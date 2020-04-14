9c2f6542ecd0083764aa287db2317d08f1d4286b
Gaubizzza <max.veramkovich@gmail.com>
2020-04-14 14:42:47 +0300

    ex.4

---
+++ src/by/veramkovich/task4/task4.java
@@ -0,0 +1,14 @@
+package by.veramkovich.task4;
+
+public class task4 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int r = 3;
+        int p = (int) 3.1415;
+        if (p*r*r >= a*b)
+            System.out.println("yeah");
+        else
+            System.out.println("bruh");
+    }
+}

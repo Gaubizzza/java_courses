54e113ec29b967299e69c285829917ccbaf5233d
Gaubizzza <max.veramkovich@gmail.com>
2020-04-14 14:32:31 +0300

    ex.3

---
+++ src/by/veramkovich/task3/task3.java
@@ -0,0 +1,12 @@
+package by.veramkovich.task3;
+
+public class task3 {
+    public static void main(String[] args) {
+        int x = 1234567;
+        if (x % 10 == 7)
+            System.out.println("yeah");
+        else
+            System.out.println("bruh");
+
+    }
+}
